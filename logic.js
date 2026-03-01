d4408413f97b5bb31781dd8cb50d9a4ca94ae006
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-01 11:59:29 +0530

    fetch all necessary details of weather

--- logic.js
+++ logic.js
@@ -32,10 +32,43 @@ timezone=auto`
         // what we need -> City, Temperature, Weather, Humidity, Wind
         let weatherObject = await fetch(url);
         let response = await weatherObject.json();
+
+        // geting the textual data from the weather code
+        let weather_code = await fetch('./weather_code.json');
+        let weather_code_response = await weather_code.json();
+        console.log(response)
+// interval: 900
+// ​
+// is_day: 1
+// ​
+// temperature: 27.2
+// ​
+// time: "2026-03-01T11:45"
+// ​
+// weathercode: 0
+// ​
+// winddirection: 291
+// ​
+// windspeed: 9.2
+
         let current_weather = response.current_weather;
         let current_weather_units = response.current_weather_units;
-        console.log(current_weather)
+
         let temperature = current_weather.temperature;
+        let temperatureUnit = current_weather_units.temperature;
+
+        let weather = weather_code_response[current_weather.weathercode];
+
+        let humidity = Math.round(response.hourly.relative_humidity_2m.reduce((curr, acc)=> curr+acc ,0)/response.hourly.relative_humidity_2m.length);
+        let humidity_unit = response.hourly_units.relative_humidity_2m;
+
+        let wind = current_weather.windspeed;
+        let wind_unit = current_weather_units.windspeed;
+
+        console.log(temperature, temperatureUnit);
+        console.log(weather);
+        console.log(humidity, humidity_unit);
+        console.log(wind, wind_unit);
 
 
     } catch (err) {
