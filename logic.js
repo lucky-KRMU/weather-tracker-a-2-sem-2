729f3122f9b57f85093a0f737f6cc8d08eeaef48
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-01 12:53:30 +0530

    add api data in DOM

--- logic.js
+++ logic.js
@@ -1,3 +1,5 @@
+let details = document.querySelector('.details');
+
 
 // this is the function to get the City Name. By Default the city's value is Delhi
 async function getCityName(cityName = "Delhi") {
@@ -7,7 +9,7 @@ async function getCityName(cityName = "Delhi") {
         let result = response.results[0]
         let latitude = result.latitude;
         let longitude = result.longitude;
-        let city = result.city;
+        let city = result.name;
         // console.log(result.name);
         // console.log(result.latitude);
         // console.log(result.longitude);
@@ -23,6 +25,7 @@ async function getWeatherDetails(cityName = 'Delhi') {
     try {
         // part of making the correct URL
         let [latitude, longitude, city] = await getCityName(cityName);
+        console.log(city)
         let url =  `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,relative_humidity_2m,weather_code&current_weather=true&
 timezone=auto`
         console.log(url);
@@ -37,20 +40,8 @@ timezone=auto`
         let weather_code = await fetch('./weather_code.json');
         let weather_code_response = await weather_code.json();
         console.log(response)
-// interval: 900
-// ​
-// is_day: 1
-// ​
-// temperature: 27.2
-// ​
-// time: "2026-03-01T11:45"
-// ​
-// weathercode: 0
-// ​
-// winddirection: 291
-// ​
-// windspeed: 9.2
 
+        // Getting the actual data
         let current_weather = response.current_weather;
         let current_weather_units = response.current_weather_units;
 
@@ -59,7 +50,7 @@ timezone=auto`
 
         let weather = weather_code_response[current_weather.weathercode];
 
-        let humidity = Math.round(response.hourly.relative_humidity_2m.reduce((curr, acc)=> curr+acc ,0)/response.hourly.relative_humidity_2m.length);
+        let humidity = Math.round(response.hourly.relative_humidity_2m.reduce((curr, acc)=> curr+acc ,0)/response.hourly.relative_humidity_2m.length); // taking the average of the humidity array and rouding it off.
         let humidity_unit = response.hourly_units.relative_humidity_2m;
 
         let wind = current_weather.windspeed;
@@ -69,6 +60,33 @@ timezone=auto`
         console.log(weather);
         console.log(humidity, humidity_unit);
         console.log(wind, wind_unit);
+
+        // appending the values in the DOM.
+        let cityDOM = document.createElement('div');
+        cityDOM.className = 'info-strip';
+        cityDOM.textContent = `City: ${city}`
+
+        let temperatureDOM = document.createElement('div');
+        temperatureDOM.className = 'info-strip';
+        temperatureDOM.textContent = `Temperature: ${temperature}${temperatureUnit}`;
+
+        let weatherDOM = document.createElement('div');
+        weatherDOM.className = 'info-strip';
+        weatherDOM.textContent = `Weather: ${weather}`;
+
+        let humidityDOM = document.createElement('div');
+        humidityDOM.className = 'info-strip';
+        humidityDOM.textContent = `Humidity: ${humidity}${humidity_unit}`;
+
+        let windDOM = document.createElement('div');
+        windDOM.className = 'info-strip';
+        windDOM.textContent = `Wind: ${wind}${wind_unit}`;
+
+        details.appendChild(cityDOM)
+        details.appendChild(temperatureDOM);
+        details.appendChild(weatherDOM);
+        details.appendChild(humidityDOM);
+        details.appendChild(windDOM);
 
 
     } catch (err) {
